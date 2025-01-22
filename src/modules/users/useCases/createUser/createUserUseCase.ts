a4d970c70ba84d4425424c96c93cfee5f9019026
Wiwie Sanjaya <wiwiewei18@gmail.com>
2025-01-22 16:57:55 +0700

    feat: create user name value object

--- src/modules/users/useCases/createUser/createUserUseCase.ts
+++ src/modules/users/useCases/createUser/createUserUseCase.ts
@@ -5,6 +5,7 @@ import { CreateUserDTO } from "./createUserDTO";
 import { CreateUserErrors } from "./createUserErrors";
 import { UserEmail } from "../../domain/userEmail";
 import { UserPassword } from "../../domain/userPassword";
+import { UserName } from "../../domain/userName";
 
 type Response = Either<
   | CreateUserErrors.EmailAlreadyExistsError
@@ -20,6 +21,7 @@ export class CreateUserUseCase
   async execute(request: CreateUserDTO): Promise<Response> {
     const emailOrError = UserEmail.create(request?.email);
     const passwordOrError = UserPassword.create({ value: request.password });
+    const usernameOrError = UserName.create({ name: request.username });
 
     return right(Result.ok<void>());
   }
